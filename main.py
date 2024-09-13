c3065ee49d15bf89d7a89a6d331085d83d1d4980
uygar <uygargurnal2@gmail.com>
2024-09-13 13:18:30 +0300

    initial f

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 df = pd.read_csv("topics1.csv")
 pdf = FPDF(orientation="P", unit="mm", format="a4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 for index, row in df.iterrows():
     pdf.add_page()
@@ -10,13 +11,21 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align="L")
     pdf.line(10, 21, 200, 21)
+    pdf.ln(265)
     pdf.set_font(family="Times", style="B", size=8)
-    pdf.set_text_color(100, 100, 100)
-    pdf.text(x=190, y=290, txt=row["Topic"])
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=1, txt=row["Topic"], ln=1, align="R")
+    for i in range(28):
+        i = i*10
+        pdf.line(10, 21+i, 200, 21+i)
     for i in range(df["Pages"][index]-1):
         pdf.add_page()
+        pdf.ln(277)
         pdf.set_font(family="Times", style="B", size=8)
-        pdf.set_text_color(100, 100, 100)
-        pdf.text(x=180, y=290, txt=row["Topic"])
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=1, txt=row["Topic"], ln=1, align="R")
+        for i in range(28):
+            i = i * 10
+            pdf.line(10, 21 + i, 200, 21 + i)
 
 pdf.output("output.pdf")
