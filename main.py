a885edef70e1e1b30fff3d020751e328eb91f609
uygar <uygargurnal2@gmail.com>
2024-09-13 12:57:52 +0300

    initial e

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align="L")
     pdf.line(10, 21, 200, 21)
+    pdf.set_font(family="Times", style="B", size=8)
+    pdf.set_text_color(100, 100, 100)
+    pdf.text(x=190, y=290, txt=row["Topic"])
+    for i in range(df["Pages"][index]-1):
+        pdf.add_page()
+        pdf.set_font(family="Times", style="B", size=8)
+        pdf.set_text_color(100, 100, 100)
+        pdf.text(x=180, y=290, txt=row["Topic"])
 
 pdf.output("output.pdf")
